5f3b78933aa407919b75db44d6bb21c503f218cf
edward-cke1110 <eddynguyen02@gmail.com>
2023-05-20 22:27:38 +1000

    fixed Suniel's work

--- app/src/main/java/com/edwardgroup/covid19recorder/AddMessageActivity.java
+++ app/src/main/java/com/edwardgroup/covid19recorder/AddMessageActivity.java
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class share_records extends AppCompatActivity {
+public class AddMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sharerecords);
     }
 }
